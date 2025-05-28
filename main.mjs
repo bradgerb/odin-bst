ecd6753af69a2aa86071c17adc7cd5c224906011
bradgerb <77023448+bradgerb@users.noreply.github.com>
2025-05-28 15:26:20 -0400

    Test depth

--- main.mjs
+++ main.mjs
@@ -53,7 +53,13 @@ prettyPrint(printTree);
 //   console.log(node.value);
 // });
 
-console.log(testTree.height(8));
-console.log(testTree.height(1));
-console.log(testTree.height(6345));
-console.log(testTree.height(92387));
+// console.log(testTree.height(8));
+// console.log(testTree.height(1));
+// console.log(testTree.height(6345));
+// console.log(testTree.height(92387));
+
+console.log(testTree.depth(8));
+console.log(testTree.depth(1));
+console.log(testTree.depth(6345));
+console.log(testTree.depth(92387));
+console.log(testTree.depth(5));
